293c82599cbf4af727c49aca1cd4e2d213153189
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-02-17 13:38:16 +0200

    Structure Done

---
+++ routes/userRoute.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const router = express.Router();
+const con = require("");
+
+router.get("/", (req, res) =>{
+    try{
+        con.query("SELECT * FROM Users", (err, results) =>{
+            if(err) throw err;
+            res.send(results);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+router.get("/:id", (req, res) =>{
+    try{
+        con.query(`SELECT FROM Users WHERE id=${req.params.id}`, (err, results) =>{
+            if(err) throw err;
+            res.send(results);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+router.post("/", (req, res) =>{
+    try{
+        con.query(`INSERT INTO Users(full_name, email, user_password, phone) VALUE ("${req.body.full_name}","${req.body.email}","${req.body.user_password}","${req.body.phone}")`, (err, results) =>{
+            if(err) throw err;
+            res.send(results);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+router.patch("/", (req, res) =>{
+    try{
+        con.query(`UPDATE Users SET full_name = "${req.body.full_name}", email = "${req.body.email}", user_password = "${req.body.user_password}", phone = "${req.body.phone}" WHERE user_id = ${req.params.id}`, (err, results) =>{
+            if(err) throw err;
+            res.send(results);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+router.put("/:id", (req, res) =>{
+    try{
+        con.query(`UPDATE Users SET full_name = "${req.body.full_name}", email = "${req.body.email}", user_password = "${req.body.user_password}", phone = "${req.body.phone}" WHERE user_id = ${req.params.id}`, (err, results) =>{
+            if(err) throw err;
+            res.send(results);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+router.delete("/:id", (req, res) =>{
+    try{
+        con.query(`DELETE FROM Users WHERE id = "${req.params.id}"`, (err, results) =>{
+            if(err) throw err;
+            res.send(results);
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send(error)
+    }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.set("port", process.env.PORT || 8000);
+app.use(express.json());
+
+let corsOption = {
+    origin: 'http://localhost:3306'
+};
+app.use(cors(corsOption));
+
+const userRouter = require('./routes/userRoute');
+// const productsRouter = require('./routes/productsRoute');
+// const loginRouter = require('./routes/loginRoute');
+// const registerRouter = require('./routes/registerRoute');
+
+app.use('/users', userRouter);
+// app.use('/products', productsRouter);
+// app.use('/login', loginRouter);
+// app.use('/register', registerRouter);
+
+app.get('/', (req, res) =>{
+    res.json({ message: "Welcome To The Store!!!"});
+});
+
+app.listen(app.get("port"), () =>{
+    console.log(`Listening For Calls On Port ${app.get("port")}`);
+    console.log("Press Ctrl+C to exit server");
+});
